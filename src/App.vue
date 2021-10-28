c3da0891bcdd164671883a39e0bda3a9e3b8c2c3
Linghanchujian <2739069093@qq.com>
2021-10-28 17:41:38 +0800

    update: https://github.com/unjs/unplugin/issues/33

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
     <div>
         <MdiAccountBox/>
         {{ title }}
-        <!-- <MdiAccountBoxOutline/> -->
+        <MdiAccountBoxOutline/>
     </div>
 </template>
 
 <script lang="ts" setup>
 import MdiAccountBox from '~icons/mdi/account-box';
-// import MdiAccountBoxOutline from '~icons/mdi/account-box-outline';
+import MdiAccountBoxOutline from '~icons/mdi/account-box-outline';
 
 const title = '这是个测试';
 </script>
